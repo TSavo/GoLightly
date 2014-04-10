2a551174b25d345a8b977e659c90e6bbde215cd4
Travis Savo <evilgenius@nefariousplan.com>
2014-04-09 23:53:33 -0700

    Adding docs

--- processor.go
+++ processor.go
@@ -6,6 +6,9 @@ import (
 	"time"
 )
 
+//A Processor contains an instruction set, some memory, a Program, an Instruction Pointer 
+//(an index for where in the program it is), and a Termination Condition which 
+//let's it know when to stop.
 type Processor struct {
 	*InstructionSet
 	Registers            Memory
@@ -19,6 +22,8 @@ type Processor struct {
 	TerminationCondition *TerminationCondition
 }
 
+
+//A Processor's cost is the number of operations it's executed + the program length + the stack length + the call stack length
 func (p *Processor) Cost() int64 {
 	progLen := int64(len(*p.Program))
 	cost := p.cost
@@ -36,19 +41,7 @@ func (p *Processor) String() string {
 		p.Cost())
 }
 
-func (t *Processor) Call(location int) {
-	t.CallStack.Push(t.InstructionPointer)
-	t.Jump(location)
-}
-
-func (t *Processor) Return() {
-	if t.CallStack.Len() > 0 {
-		t.InstructionPointer, _ = t.CallStack.Pop()
-	}
-	t.InstructionPointer++
-
-}
-
+//Change the instruction pointer to the specified index
 func (t *Processor) Jump(jump int) {
 	t.InstructionPointer = jump
 	if t.InstructionPointer < 0 {
@@ -57,6 +50,21 @@ func (t *Processor) Jump(jump int) {
 	t.InstructionPointer = t.InstructionPointer % len(*t.Program)
 }
 
+//Push the current instruction pointer onto the call stack, and jump to the location in the program specified.
+func (t *Processor) Call(location int) {
+	t.CallStack.Push(t.InstructionPointer)
+	t.Jump(location)
+}
+
+//Pop the top of the call stack, and jump to that value.
+func (t *Processor) Return() {
+	if t.CallStack.Len() > 0 {
+		t.InstructionPointer, _ = t.CallStack.Pop()
+	}
+	t.InstructionPointer++
+}
+
+//Create a new Processor with a memory of length 'registers', an instruction set, a heap, and a termination condition
 func NewProcessor(registers int, instructions *InstructionSet, heap *Memory, stop *TerminationCondition) *Processor {
 	p := new(Processor)
 	p.TerminationCondition = stop
@@ -70,27 +78,34 @@ func NewProcessor(registers int, instructions *InstructionSet, heap *Memory, sto
 	return p
 }
 
+//Load a program into the processor. This has the side effect of setting the instruction pointer to 0.
 func (p *Processor) LoadProgram(program *Program) {
 	pr := make(Program, len(*program))
 	p.Program = &pr
 	copy(*p.Program, *program)
+	p.InstructionPointer = 0
 }
 
+//Compile a program from a string and load it into the processor.
 func (p *Processor) CompileAndLoad(prog string) {
 	p.LoadProgram(p.InstructionSet.CompileProgram(prog))
 }
 
+//Decompile the program on the processor into a string.
 func (p *Processor) GetProgramString() string {
 	return p.Program.Decompile()
 }
 
+//Zero the registers, reset the stack and the call stack, and set the cost and instruction pointer to 0
 func (p *Processor) Reset() {
 	p.Registers.Zero()
 	p.CallStack.Reallocate(0)
+	p.Stack.Reallocate(0)
 	p.InstructionPointer = 0
 	p.cost = 0
 }
 
+//Execute the instruction currently pointed at by the Instruction Pointer
 func (p *Processor) Execute() {
 	if len(*p.Program) == 0 {
 		return
@@ -105,6 +120,7 @@ func (p *Processor) Execute() {
 	p.InstructionPointer += opcode.Instruction.Movement
 }
 
+//Start running the program. This won't return until the TerminationCondition.ShouldTerminate() returns true.
 func (p *Processor) Run() {
 	p.StartTime = time.Now().UnixNano()
 	for {
